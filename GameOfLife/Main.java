99f8484a43848d233849ebf9c963bf3c0089cefd
Kamil <uhkamil@gmail.com>
2025-11-06 16:39:42 +0100

    Initialized classes, implemented DisplayBoard

--- GameOfLife/Main.java
+++ GameOfLife/Main.java
@@ -1,3 +1,57 @@
-void main(){
-    System.out.println("Hello world");
+import java.util.Scanner;
+
+class GameOfLife {
+    static void Rules() {
+
+    }
 }
+
+class Board {
+    int SizeX;
+    int SizeY;
+
+    final char zero = '□';
+    final char one = '■';
+
+    public void DisplayBoard () {
+        for (int i = 1; i <= SizeX; i++) {
+            for (int j = 1; j <= SizeY; j++){
+                print(zero + " ");
+            }
+            print("\n");
+        }
+    }
+
+    public Board (int SizeX, int SizeY) {
+        this.SizeX = SizeX;
+        this.SizeY = SizeY;
+
+        DisplayBoard();
+
+        int[][] Plansza = new int[SizeX][SizeY];
+
+
+    }
+
+
+
+}
+
+static void println(Object s) {
+    System.out.println(s);
+}
+
+static void print(Object s) {
+    System.out.print(s);
+}
+
+
+void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    println("Podaj rozmiar x tabeli");
+    int x = scanner.nextInt();
+    println("Podaj rozmiar y tabeli");
+    int y = scanner.nextInt();
+    Board board = new Board(x, y);
+}
+
---
+++ GameOfLife/main.bat
@@ -0,0 +1,3 @@
+chcp 65001
+javac -encoding UTF-8 Main.java
+java -Dfile.encoding=UTF-8 Main.java
