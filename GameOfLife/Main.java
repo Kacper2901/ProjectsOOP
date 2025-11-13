da33f0c42d5c961282d0bd0242d1f1ba23ac61fd
Kacper Gęśla <kacper.gesla29@gmail.com>
2025-11-13 16:34:19 +0100

    I was working on creating a game board

--- GameOfLife/Main.java
+++ GameOfLife/Main.java
@@ -1,34 +1,60 @@
 import java.util.Scanner;
 
 class GameOfLife {
-    static void Rules() {
+    Board b;
 
+    public static int countLivingCells(Board b){
+        for(int i = 0; i < b.sizeX; i++){
+            continue;
+        }
+        return 0;
     }
 }
 
 class Board {
-    int SizeX;
-    int SizeY;
+    int sizeX;
+    int sizeY;
+    char[][] plansza = new char[sizeX][sizeY];
 
     final char zero = '□';
     final char one = '■';
 
     public void DisplayBoard () {
-        for (int i = 1; i <= SizeX; i++) {
-            for (int j = 1; j <= SizeY; j++){
-                print(zero + " ");
+        for (int i = 1; i <= sizeX; i++) {
+            for (int j = 1; j <= sizeY; j++){
+                print(plansza[i][j] + " ");
             }
             print("\n");
         }
     }
 
     public Board (int SizeX, int SizeY) {
-        this.SizeX = SizeX;
-        this.SizeY = SizeY;
+        this.sizeX = SizeX;
+        this.sizeY = SizeY;
+        String flag = " ";
+        int x;
+        int y;
+
+
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < this.sizeX; i++){
+            for(int j = 0; j < this.sizeY; j++){
+                plansza[i][j] = zero;
+            }
+        }
+
+        println("Enter x,y of living cell (0<x<" + (this.sizeX-1) +", 0<y<" + (this.sizeY - 1));
+        while(!flag.equals("n")){
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            plansza[x][y] = one;
+
+            println("Do you want to add another one? y/n");
+            flag = scanner.nextLine();
+        }
 
         DisplayBoard();
 
-        int[][] Plansza = new int[SizeX][SizeY];
 
 
     }
@@ -53,5 +79,6 @@ void main(String[] args) {
     println("Podaj rozmiar y tabeli");
     int y = scanner.nextInt();
     Board board = new Board(x, y);
+    board.DisplayBoard();
 }
 
